5726929fe3ae3000ca48602ed10aff01ceb364da
Aagat Ghimire <aagat.gh001@gmail.com>
2022-09-29 20:14:57 +0545

    JS for Food/Drink updated and Working

--- script.js
+++ script.js
@@ -1,30 +1,38 @@
 //Online Canteen System
 
-// Code for plus/minus
+// Code for plus/minus starts here
 
-    var minusBtn = document.getElementsByClassName("minus");
-     var plusBtn = document.getElementsByClassName("plus");
-     var numberPlace = document.getElementsByClassName("numberPlace");
-     var number = 0;
-     var min = 0;
+var incBtn=document.getElementsByClassName('plus');
+var decBtn=document.getElementsByClassName('minus');
 
-    // Only works for 1 element becuase ID must be unique
-    /*
-    var minusBtn = document.getElementById("minus");
-    var plusBtn = document.getElementById("plus");
-    var numberPlace = document.getElementById("numberPlace");
-    var number = 0;
-    var min = 0;
-    */
-    
-    minusBtn.onclick = function () {
-        if (number > min) {
-            number = number - 1;
-            numberPlace.innerText = number;
+//plus
+for(var i=0;i<incBtn.length;i++)
+{
+    var button=incBtn[i];
+    button.addEventListener('click',function(event){
+        var buttonClicked=event.target;
+        var input=buttonClicked.parentElement.children[1];
+        var inputValue=input.value;
+        var newValue=parseInt(inputValue)+1;
+        // numberPlace.innerText = newValue;
+        input.value=newValue;
+    })
+}
+
+//minus
+for(var i=0;i<decBtn.length;i++)
+{
+    var button=decBtn[i];
+    button.addEventListener('click',function(event){
+        var buttonClicked=event.target;
+        var input=buttonClicked.parentElement.children[1];
+        var inputValue=input.value;
+        if(inputValue>0)
+        {
+        var newValue=parseInt(inputValue)-1;
+        // numberPlace.innerText = newValue;
+        input.value=newValue;
         }
-    }
-
-    plusBtn.onclick = function () {
-            number = number + 1;
-            numberPlace.innerText = number;
-    }
+    })
+}
+// Code for plus/minus ends here
