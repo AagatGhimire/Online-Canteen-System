577a9f22de264b82f3e02440120ef65739def521
Aagat Ghimire <aagat.gh001@gmail.com>
2022-10-17 17:37:04 +0545

    Butons Modified

--- script.js
+++ script.js
@@ -1,41 +1,6 @@
 //Online Canteen System
 
 
-// Code for plus/minus starts here
-
-var incBtn = document.getElementsByClassName('plus');
-var decBtn = document.getElementsByClassName('minus');
-
-//plus
-for (var i = 0; i < incBtn.length; i++) {
-    var button = incBtn[i];
-    button.addEventListener('click', function (event) {
-        // echo('ok');
-        var buttonClicked = event.target;
-        var input = buttonClicked.parentElement.children[1];
-        var inputValue = input.value;
-        var newValue = parseInt(inputValue) + 1;
-        // numberPlace.innerText = newValue;
-        input.value = newValue;
-    })
-}
-
-//minus
-for (var i = 0; i < decBtn.length; i++) {
-    var button = decBtn[i];
-    button.addEventListener('click', function (event) {
-        var buttonClicked = event.target;
-        var input = buttonClicked.parentElement.children[1];
-        var inputValue = input.value;
-        if (inputValue > 0) {
-            var newValue = parseInt(inputValue) - 1;
-            // numberPlace.innerText = newValue;
-            input.value = newValue;
-        }
-    })
-}
-// Code for plus/minus ends here
-
 // Code for cart starts here
 
 const cartIcon = document.querySelector('.fa-basket-shopping');
@@ -96,8 +61,26 @@ function requestFood() {
                     nPlace.value='0';
                     pb.textContent='+';
                     mb.textContent='-';
-                    pb.addEventListener('click', pbclicked)
-                    mb.addEventListener('click', mbclicked)
+                    
+                    //Function for buttons
+                        pb.addEventListener('click', function (event) {
+                            var buttonClicked = event.target;
+                            var input = buttonClicked.parentElement.children[1];
+                            var inputValue = input.value;
+                            var newValue = parseInt(inputValue) + 1;
+                            input.value = newValue;
+                        })
+
+                        mb.addEventListener('click', function (event) {
+                            var buttonClicked = event.target;
+                            var input = buttonClicked.parentElement.children[1];
+                            var inputValue = input.value;
+                            if (inputValue > 0) {
+                                var newValue = parseInt(inputValue) - 1;
+                                input.value = newValue;
+                            }
+                        })
+
                     pmb.appendChild(mb);
                     pmb.appendChild(nPlace);
                     pmb.appendChild(pb);
@@ -150,8 +133,26 @@ function requestDrinks() {
                     nPlace.value='0';
                     pb.textContent='+';
                     mb.textContent='-';
-                    pb.addEventListener('click', pbclicked)
-                    mb.addEventListener('click', mbclicked)
+
+                    //Function for buttons
+                    pb.addEventListener('click', function (event) {
+                        var buttonClicked = event.target;
+                        var input = buttonClicked.parentElement.children[1];
+                        var inputValue = input.value;
+                        var newValue = parseInt(inputValue) + 1;
+                        input.value = newValue;
+                    })
+
+                    mb.addEventListener('click', function (event) {
+                        var buttonClicked = event.target;
+                        var input = buttonClicked.parentElement.children[1];
+                        var inputValue = input.value;
+                        if (inputValue > 0) {
+                            var newValue = parseInt(inputValue) - 1;
+                            input.value = newValue;
+                        }
+                    })
+
                     pmb.appendChild(mb);
                     pmb.appendChild(nPlace);
                     pmb.appendChild(pb);
@@ -167,24 +168,14 @@ function requestDrinks() {
         .catch(err => console.log(err));
 }
 
-function pbclicked(){
-    console.log('ok',this)
-        // var buttonClicked = event.target;
-        // var input = buttonClicked.parentElement.children[1];
-        // var inputValue = input.value;
-        // var newValue = parseInt(inputValue) + 1;
-        // // numberPlace.innerText = newValue;
-        // input.value = newValue;
-}
-function mbclicked(){
-    console.log('hmm')
-}
+
 // Code for backend stuff ends here
 
 
 
 
 //Filler:
+
 // const foodSec= document.querySelector('#food_section');
 //                 const foodDiv= document.createElement('div');
 //                 let foodlist;
@@ -216,6 +207,7 @@ function mbclicked(){
 //                 console.log(foodSec)
 
 //filler: method 2
+
 // const drink = data.drinks
 //                     .map(drinkData => {
 //                         return `
@@ -235,3 +227,40 @@ function mbclicked(){
 //                 // console.log(drink);
 //                 // document.querySelector('#drink_section').innerHTML = drink;
 //                 document.querySelector('#drink_section').insertAdjacentHTML("afterbegin",drink)
+
+//  filler:Plus Minus Button
+
+// // Code for plus/minus starts here
+
+// var incBtn = document.getElementsByClassName('plus');
+// var decBtn = document.getElementsByClassName('minus');
+
+// //plus
+// for (var i = 0; i < incBtn.length; i++) {
+//     var button = incBtn[i];
+//     button.addEventListener('click', function (event) {
+//         // echo('ok');
+//         var buttonClicked = event.target;
+//         var input = buttonClicked.parentElement.children[1];
+//         var inputValue = input.value;
+//         var newValue = parseInt(inputValue) + 1;
+//         // numberPlace.innerText = newValue;
+//         input.value = newValue;
+//     })
+// }
+
+// //minus
+// for (var i = 0; i < decBtn.length; i++) {
+//     var button = decBtn[i];
+//     button.addEventListener('click', function (event) {
+//         var buttonClicked = event.target;
+//         var input = buttonClicked.parentElement.children[1];
+//         var inputValue = input.value;
+//         if (inputValue > 0) {
+//             var newValue = parseInt(inputValue) - 1;
+//             // numberPlace.innerText = newValue;
+//             input.value = newValue;
+//         }
+//     })
+// }
+// // Code for plus/minus ends here
